574483a059a62660bc3dc73e3a3bd523648a6c7c
YarivSwid <yarivswid@gmail.com>
2022-06-18 12:22:11 +0300

    anova

--- T test.R
+++ T test.R
@@ -21,3 +21,14 @@ print(Test2)
 new <- table(table$Province,table$Category)
 ftable(new)
 summary(new)
+##########################################################################
+
+filePath=choose.files() 
+anov<-read.csv(filePath,header=TRUE)
+revenueLost <- anov$Revenue.y1 - anov$Revenue.y3
+print(revenueLost)
+boxplot(revenueLost~anov$Quarter,data = anov)
+anov$Quarter <- as.factor(anov$Quarter)
+anova(lm(revenueLost ~ anov$Quarter,anov))
+
+
